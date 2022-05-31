7047d4ff8ffea28a1b1bb89267f6d85480463494
Gabriel Gomes <39136800+gomessgbr@users.noreply.github.com>
2022-05-31 20:26:35 -0300

    delete useless resources and add api link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+
+const API = "https://pluga.co/ferramentas_search.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
