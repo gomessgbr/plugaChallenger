dd00a425c3a1e97d9259d35fec2af9fd6a00733f
Gabriel Gomes <39136800+gomessgbr@users.noreply.github.com>
2022-05-31 20:35:15 -0300

    fetching data from API pluga

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
-
-const API = "https://pluga.co/ferramentas_search.json";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch("https://pluga.co/ferramentas_search.json")
+      .then((response) => response.json())
+      .then((response) => setData(response));
+  }, []);
   return <></>;
 }
 
