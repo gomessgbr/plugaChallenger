35a73f207b191c4b18132fa395027d370d5d15b7
Gabriel Gomes <39136800+gomessgbr@users.noreply.github.com>
2022-05-31 21:13:27 -0300

    render on screen icons and buttons from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+
+  const handleTeste = () => {};
+
   useEffect(() => {
     fetch("https://pluga.co/ferramentas_search.json")
       .then((response) => response.json())
       .then((response) => setData(response));
   }, []);
-  return <></>;
+  return (
+    <>
+      {data.map((item) => (
+        <div style={{ height: 200, width: 200 }}>
+          <a href={item.link}>
+            <text>{item.name}</text>
+            <img src={item.icon} alt="Teste" />
+          </a>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
