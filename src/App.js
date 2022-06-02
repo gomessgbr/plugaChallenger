b6cc18661f6c8bcf474a0af07e70ba81e8fb7bda
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-06-02 16:27:15 -0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React, { useEffect, useState } from "react";
-
+import { Cards } from "./components/Cards";
 function App() {
+  let tamanhoPage = 12;
+  let page = 0;
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
+  const [itensPerPage, setItensPerPage] = useState(12);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const pages = Math.ceil(data.length / itensPerPage);
+  const startIndex = currentPage * itensPerPage;
+  const endIndex = startIndex + itensPerPage;
+  const currentItem = data.slice(startIndex, endIndex);
+
+  const searchedNames = currentItem.filter(({ name }) => {
+    return name.includes(search);
+  });
+
+  const sliceData = () => {};
 
   const fetchData = () => {
     fetch("https://pluga.co/ferramentas_search.json")
       .then((response) => response.json())
       .then((response) => setData(response));
   };
-  const searchedNames = data.filter(({ name }) => {
-    return name.includes(search);
-  });
-
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <>
+      <div>
+        {Array.from(Array(pages), (item, index) => {
+          return (
+            <button
+              value={index}
+              onClick={(event) => setCurrentPage(Number(event.target.value))}
+            >
+              {index}
+            </button>
+          );
+        })}
+      </div>
       <input
         type="search"
         onChange={(event) => {
@@ -30,23 +53,16 @@ function App() {
       <div
         style={{
           backgroundColor: "blue",
+          justifyContent: "space-between",
         }}
       >
         {searchedNames.map((item) => (
-          <div
-            style={{
-              maxHeight: 200,
-              maxWidth: 300,
-              backgroundColor: "#01321A",
-              justifyContent: "space-around",
-            }}
+          <Cards
+            appIcon={item.icon}
+            appLink={item.link}
+            appName={item.name}
             key={item.app_id}
-          >
-            <a href={item.link} style={{}}>
-              <p>{item.name}</p>
-              <img src={item.icon} alt="Teste" />
-            </a>
-          </div>
+          />
         ))}
       </div>
     </>
