7915e378b08622bb233ef0ebadb759833f148dab
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-06-02 16:28:16 -0300

    initial component pagination

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export function Pagination(){
+    let tamanhoPage=12
+    let page = 0
+
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
