55cb4d740e8fc469be7a48287c87a539382d59de
Gabriel Gomes <39136800+gomessgbr@users.noreply.github.com>
2022-06-02 23:15:32 -0300

    modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 
-export default function Modal() {
+import "./styles.css";
+export default function Modal({ itemClicked, onClickEsc }) {
   return (
     <>
-      <div>
-        <span></span>
+      <div className="modal-overlay">
+        <div className="modal-container">
+          <div className="modal-icons">
+            <img src={itemClicked.icon} alt="logo app" />
+          </div>
+          <div className="modal-txt">
+            <span className=".modal-name">{itemClicked.name}</span>
+
+            <a href={itemClicked.link} className="btn-acess">
+              Acessar
+            </a>
+          </div>
+        </div>
+
+        <div className="esc">
+          <button className="xbtn" onClick={onClickEsc}>
+            x
+          </button>
+        </div>
       </div>
     </>
   );
